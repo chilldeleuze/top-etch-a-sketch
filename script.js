772be8cf50a74b086af321ff906063d115dd6680
chilldeleuze <16662248+chilldeleuze@users.noreply.github.com>
2024-10-04 16:56:51 +0200

    added custom grid creation

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 
 
 const canvas = document.getElementById("canvas");
+const canvasWidth = canvas.getBoundingClientRect().width;
+let pixelNumber = 16;
+const button = document.getElementById("gridsize-button")
 
-function prepareInitialGrid() {
-    for (let i = 0; i < 16*16; i++) {
+console.log(canvasWidth);
+console.log("hello")
+
+button.addEventListener("click", () => {
+    console.log("click");
+    let i = 0;
+    while (i < 3) {
+        let input = window.prompt("Enter Gridsize");
+        let num = parseInt(input);
+        if (Number.isInteger(num) && num <= 100) {
+            pixelNumber = num;
+            break;
+        };
+        i++;
+    };
+    console.log("pixelnumer:" + pixelNumber)
+    clearGrid();
+    createGrid(pixelNumber);
+
+})
+
+function clearGrid() {
+    canvas.replaceChildren();
+}
+
+function createGrid(pixelNumber) {
+    let pixelSize = `${canvasWidth / pixelNumber}px`
+    for (let i = 0; i < pixelNumber * pixelNumber; i++) {
         const div = document.createElement("div");
         div.style.backgroundColor = "white";
-        div.style.width = "20px";
-        div.style.height = "20px";
+        div.style.width = pixelSize;
+        div.style.height = pixelSize;
         div.classList.add("pixel");
         canvas.appendChild(div);
     }
 }
 
-prepareInitialGrid()
+createGrid(16)
+
+canvas.addEventListener("mouseover", (e) => {
+    let target = e.target;
+    target.style.backgroundColor = "black";
+})
 
